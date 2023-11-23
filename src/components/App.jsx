4e79727ba968bda3df087a12ba73e6671cfc773a
Lakshay Batra <lakshay@affinityanswers.com>
2023-11-23 14:05:12 +0530

    feat: Stores user notes in local storage of the browser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [cards,setCards] = useState([]);
+  const [cards, setCards] = useState(() => {
+    const storedCards = localStorage.getItem("cards");
+    return storedCards ? JSON.parse(storedCards) : [];
+  });
+
+  useEffect(() => {
+    if (cards?.length) {
+      console.log(cards);
+      localStorage.setItem("cards", JSON.stringify(cards));
+    }
+  }, [cards]);
 
   function addItem(item) {
-    setCards(prevValue => {
+    setCards((prevValue) => {
       return [...prevValue, item];
     });
   }
 
   function deleteItem(id) {
-    setCards(prevValue => {
-      return(prevValue.filter((card,index) => {
+    setCards((prevValue) => {
+      return prevValue.filter((card, index) => {
         return index !== id;
-      }));
+      });
     });
   }
 
@@ -25,8 +35,16 @@ function App() {
     <div>
       <Header />
       <CreateArea addItemProp={addItem} />
-      {cards.map((card,index) => {
-        return <Note key={index} id={index} title={card.title} content={card.content} deleteItemProp={deleteItem} />
+      {cards.map((card, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={card.title}
+            content={card.content}
+            deleteItemProp={deleteItem}
+          />
+        );
       })}
       <Footer />
     </div>
